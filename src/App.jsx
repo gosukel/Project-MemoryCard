0c2cbe3f054490587d6461ea23e3de596c7c4f15
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-06-09 21:23:50 -0400

    remove import line from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import Header from "./components/header";
 import CardGrid from "./components/CardGrid";
 import levelData from "./data/data";
